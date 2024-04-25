191c458a3ad1b884e844e53aa16411bd6e747a99
GabrielaCorderoT <di22146@dasc.edu.mx>
2024-04-25 11:43:25 -0600

    Actualizacion1

--- herencia/Form1.Designer.cs
+++ herencia/Form1.Designer.cs
@@ -159,7 +159,7 @@ namespace herencia
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(798, 300);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
